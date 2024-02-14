a0da4f984b94c329d98f887f76251ac36777768e
Alexander <finods@gmail.com>
2024-02-14 19:13:17 +0200

    update

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,33 @@
+import styles from './Profile.module.css';
+
+const Profile = ({ username, tag, location, avatar, stats }) => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.profile}>
+        <div className={styles.description}>
+          <img src={avatar} alt="User avatar" className={styles.avatar} />
+          <p className={styles.name}>{username}</p>
+          <p className={styles.txtGrey}>@{tag}</p>
+          <p className={styles.txtGrey}>{location}</p>
+        </div>
+
+        <ul className={styles.stats}>
+          <li>
+            <span className="label">Followers</span>
+            <span className={styles.quantity}>{stats.followers}</span>
+          </li>
+          <li>
+            <span className="label">Views</span>
+            <span className={styles.quantity}>{stats.views}</span>
+          </li>
+          <li>
+            <span className="label">Likes</span>
+            <span className={styles.quantity}>{stats.likes}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,19 @@
+import styles from './Statistics.module.css';
+
+const Statistics = ({ title, stats }) => {
+  const elements = stats.map(item => (
+    <li className={styles.item} key={item.id}>
+      <span className="label">{item.label}</span>
+      <span className="percentage">{item.percentage}%</span>
+    </li>
+  ));
+
+  return (
+    <div className={styles.statistics}>
+      <h2 className={styles.title}>{title}</h2>
+      <ul className={styles.statList}>{elements}</ul>
+    </div>
+  );
+};
+
+export default Statistics;
